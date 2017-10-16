django-extensions/django-extensions

commit cd84db2e8072bfca6664d5f005836532fa0afe91

python3 fixes thanks to @mikoim

django_extensions/management/commands/mail_debug.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from __future__ import print_function
+
 import asyncore
 import sys
 from logging import getLogger
@@ -14,7 +16,7 @@ logger = getLogger(__name__)
 class ExtensionDebuggingServer(SMTPServer):
     """Duplication of smtpd.DebuggingServer, but using logging instead of print."""
     # Do something with the gathered message
-    def process_message(self, peer, mailfrom, rcpttos, data):
+    def process_message(self, peer, mailfrom, rcpttos, data, **kwargs):
         """Output will be sent to the module logger at INFO level."""
         inheaders = 1
         lines = data.split('\n')
@@ -77,7 +79,7 @@ class Command(BaseCommand):
             quit_command = (sys.platform == 'win32') and 'CTRL-BREAK' or 'CONTROL-C'
             print("Now accepting mail at %s:%s -- use %s to quit" % (addr, port, quit_command))
 
-            ExtensionDebuggingServer((addr, port), None)
+            ExtensionDebuggingServer((addr, port), None, decode_data=True)
             asyncore.loop()
 
         try:

joao-marcos1/extenstions-django

commit 168338daf2ece641902e1a8c8bf9d959c7c819ca

Fix regression in mail_debug for older Pythons
Regression in cd84db2e8072bfca6664d5f005836532fa0afe91

django_extensions/management/commands/mail_debug.py
@@ -78,8 +78,10 @@ class Command(BaseCommand):
         def inner_run():
             quit_command = (sys.platform == 'win32') and 'CTRL-BREAK' or 'CONTROL-C'
             print("Now accepting mail at %s:%s -- use %s to quit" % (addr, port, quit_command))
-
-            ExtensionDebuggingServer((addr, port), None, decode_data=True)
+            if sys.version_info < (3, 5):
+                ExtensionDebuggingServer((addr, port), None)
+            else:
+                ExtensionDebuggingServer((addr, port), None, decode_data=True)
             asyncore.loop()
 
         try:

tests/management/commands/test_mail_debug.py
@@ -1,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import mock
+from django_extensions.management.commands import mail_debug
+
+
+def test_initialize_mail_server():
+    with mock.patch('django_extensions.management.commands.mail_debug.asyncore.loop') as loop:
+        command = mail_debug.Command()
+        command.run_from_argv(['manage.py', 'mail_debug'])
+        assert loop.called, 'asyncore.loop was not called'