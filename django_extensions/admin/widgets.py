django-extensions/django-extensions

commit ea680debee894474a5cd16fbbfaabfe72ada6c2d

Replace remote_field.to with remote_field.remote_model

django_extensions/admin/widgets.py
@@ -35,7 +35,7 @@ class ForeignKeySearchInput(ForeignKeyRawIdWidget):
 
     def label_for_value(self, value):
         key = self.rel.get_related_field().name
-        obj = self.rel.to._default_manager.get(**{key: value})
+        obj = self.rel.related_model._default_manager.get(**{key: value})
 
         return Truncator(obj).words(14, truncate='...')
 
@@ -46,7 +46,7 @@ class ForeignKeySearchInput(ForeignKeyRawIdWidget):
     def render(self, name, value, attrs=None):
         if attrs is None:
             attrs = {}
-        opts = self.rel.to._meta
+        opts = self.rel.related_model._meta
         app_label = opts.app_label
         model_name = opts.object_name.lower()
         related_url = reverse('admin:%s_%s_changelist' % (app_label, model_name))

joao-marcos1/extenstions-django

commit 9c074f6fd93520e834b65d9dc1c2a453c532c95d

Fix https://github.com/django-extensions/django-extensions/issues/1220 by replacing `self.rel.to` to `self.rel.model` (as mentionned in the deprecation warning of https://github.com/django/django/blob/1.11.15/django/db/models/fields/reverse_related.py#L60), as opposed to using `self.rel.related_model` which is a bug introduced in https://github.com/django-extensions/django-extensions/commit/ea680debee894474a5cd16fbbfaabfe72ada6c2d.

django_extensions/admin/widgets.py
@@ -35,7 +35,7 @@ class ForeignKeySearchInput(ForeignKeyRawIdWidget):
 
     def label_for_value(self, value):
         key = self.rel.get_related_field().name
-        obj = self.rel.related_model._default_manager.get(**{key: value})
+        obj = self.rel.model._default_manager.get(**{key: value})
 
         return Truncator(obj).words(14, truncate='...')
 
@@ -46,7 +46,7 @@ class ForeignKeySearchInput(ForeignKeyRawIdWidget):
     def render(self, name, value, attrs=None):
         if attrs is None:
             attrs = {}
-        opts = self.rel.related_model._meta
+        opts = self.rel.model._meta
         app_label = opts.app_label
         model_name = opts.object_name.lower()
         related_url = reverse('admin:%s_%s_changelist' % (app_label, model_name))